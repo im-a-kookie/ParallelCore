ecdbd54d3db598e1ce6caf38211429d39286b21b
im-a-kookie <katherinesprod@gmail.com>
2024-12-01 18:54:50 +1030

    Fixed some delegate construction
    - Can now access return and data types for validating and sanitizing parameters

--- Containers/Emission/DelegateBuilder.cs
+++ Containers/Emission/DelegateBuilder.cs
@@ -1,7 +1,9 @@
 ﻿using Containers.Models;
 using Containers.Signals;
+using System.Diagnostics;
 using System.Reflection;
 using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
 using System.Text;
 using static Containers.Emission.ParameterHelper;
 
@@ -20,17 +22,22 @@ namespace Containers.Emission
             return;
         }
 
-        public class BuilderContext<T>
+        public static void DWrite(string s)
+        {
+            Debug.WriteLine(s);
+        }
+
+        public class BuilderContext<M, T>
         {
             /// <summary>
             /// Whether the target method is static
             /// </summary>
             public bool IsStatic { get; private set; }
 
-            /// <summary>
-            /// The calling object, or null (expects null if IsStatic)
-            /// </summary>
-            public Model? Caller { get; private set; }
+            ///// <summary>
+            ///// The calling object, or null (expects null if IsStatic)
+            ///// </summary>
+            //public Model? Caller { get; private set; }
 
             /// <summary>
             /// The return type of the entry delegate
@@ -62,13 +69,17 @@ namespace Containers.Emission
             /// </summary>
             public MethodInfo Target;
 
+            /// <summary>
+            /// The type of the data object mapped to this context, if any, or the first.
+            /// </summary>
+            public Type DataType = typeof(object);
 
             /// <summary>
             /// Creates the builder context from the given arguments
             /// </summary>
             /// <param name="caller"></param>
             /// <param name="target"></param>
-            public BuilderContext(Model? caller, MethodInfo target)
+            public BuilderContext(MethodInfo target)
             {
 
                 if (!typeof(T).IsSubclassOf(typeof(Delegate)))
@@ -76,11 +87,18 @@ namespace Containers.Emission
                     throw new ArgumentException("The provided type must be a Delegate!");
                 }
 
-                this.Caller = caller;
+                if(target.DeclaringType == null)
+                {
+                    throw new ArgumentException("The target must have a declaring type!");
+                }
+
+                if(!target.DeclaringType.IsAssignableTo(typeof(M)))
+                {
+                    throw new ArgumentException($"The target type must be of {typeof(M)}");
+                }
+
                 this.IsStatic = target.IsStatic;
                 this.Target = target;
-
-                if (IsStatic) this.Caller = null;
 
                 // Extract the information that we need
                 var entrySignature = ParameterHelper.GetDelegateSignature(typeof(T));
@@ -97,8 +115,18 @@ namespace Containers.Emission
                 //validate the target parameters
                 ValidateStaticTargetParams();
 
+
+                foreach(var m in Mappings)
+                {
+                    if(m.src == 0)
+                    {
+                        DataType = TargetParams[m.dst];
+                        break;
+                    }
+                }
+
                 //and adjust them for virtual signature changes
-                AdjustForVirtualSignature();
+                //AdjustForVirtualSignature();
             }
 
             /// <summary>
@@ -161,7 +189,7 @@ namespace Containers.Emission
                 var realParams = new Type[EntryParams.Length + 1];
                 Array.Copy(EntryParams, 0, realParams, 1, EntryParams.Length);
                 // now we can just set it to the caller or otherwise an object, it's always a ref type so it's not a huge deal
-                realParams[0] = Caller?.GetType() ?? typeof(object);
+                realParams[0] = Target.DeclaringType!;
                 // And switch it into the EntryParams
                 EntryParams = realParams;
 
@@ -189,7 +217,7 @@ namespace Containers.Emission
                         name: $"Callback_{Target.Name}",
                         returnType: EntryReturn,
                         parameterTypes: EntryParams,
-                        m: Caller?.GetType().Module ?? typeof(Model).Module,
+                        m: Target.DeclaringType!.Module,
                         skipVisibility: true
                         );
                 }
@@ -208,41 +236,37 @@ namespace Containers.Emission
         /// <param name="target"></param>
         /// <exception cref="ArgumentNullException"></exception>
         /// <exception cref="InvalidOperationException"></exception>
-        private static void ValidateInputs(Model? caller, MethodInfo target)
+        private static void ValidateInputs(MethodInfo target)
         {
             if (target == null)
                 throw new ArgumentNullException("Target method must be defined!");
 
-            if (!target.IsStatic && caller == null)
-                throw new InvalidOperationException("A host must be provided for non-static methods");
-
-            // static method with an instance given, not a fatal error but not great either
-            else if (target.IsStatic && caller != null)
-            {
-                // Spit out a warning
-                // Since this *may* be intended behaviour, but could also lead to unintended behaviour
-                Logger.Default.WriteBlock(
-                    $"Static method declaration provided with instance reference",
-                    $"Method:    {target.DeclaringType?.Name ?? "<Unknown>"}.{target.Name}\n" +
-                    $"Instance:  {caller.GetType().Name}\n" +
-                    $"The instance reference will be discarded.",
-                    level: LogLevel.Warn);
-            }
         }
-
-
 
         /// <summary>
         /// Generates an EndpointCallback delegate which will invoke the target method
         /// </summary>
         /// <param name="target"></param>
         /// <returns></returns>
-        public static Router.EndpointCallback CreateCallbackDelegate(Model? caller, MethodInfo target)
+        public static Router.EndpointCallback CreateCallbackDelegate(MethodInfo target)
+        {
+            return CreateCallbackDelegate(target, out var context);
+        }
+
+        /// <summary>
+        /// Generates an EndpointCallback delegate which will invoke the target method
+        /// </summary>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public static Router.EndpointCallback CreateCallbackDelegate(
+            MethodInfo target, 
+            out BuilderContext<Model, Router.EndpointCallback>? context)
         {
             // first, ensure input validity
-            ValidateInputs(caller, target);
+            ValidateInputs(target);
+
             // Build the context for the creation
-            var context = new BuilderContext<Router.EndpointCallback>(caller, target);
+            context = new BuilderContext<Model, Router.EndpointCallback>(target);
             var dynamicMethod = context.CreateDynamicMethod(); // makes it easy to get the dynamic method
 
             //now get the il generator
@@ -262,8 +286,13 @@ namespace Containers.Emission
             // (host is now not null)
             if (!target.IsStatic)
             {
-                il.Emit(OpCodes.Ldarg_0);
-                il.Emit(OpCodes.Castclass, context.Caller!.GetType());
+                // Find argument of model
+                int n = Array.IndexOf(context.EntryParams, typeof(Model));
+                if (n >= 0)
+                {
+                    il.Emit(OpCodes.Ldarg_S, n);
+                    il.Emit(OpCodes.Castclass, context.Target.DeclaringType!);
+                }
             }
 
             foreach (var map in context.Mappings)
@@ -283,12 +312,15 @@ namespace Containers.Emission
 
             // whewwww now we can make the call
             il.EmitCall(context.IsStatic ? OpCodes.Call : OpCodes.Callvirt, context.Target, null);
+            DWrite($"Call      {context.Target.Name}");
             EmitReturnType(il, context);
 
             // -- We have fully generated the IL for the dynamic method --
 
             // So create the delegate and send it back
-            return (Router.EndpointCallback)dynamicMethod.CreateDelegate(typeof(Router.EndpointCallback), context.Caller);
+            // The delegate itself is statically typed
+            // And uses the Model parameter to do the thing
+            return (Router.EndpointCallback)dynamicMethod.CreateDelegate(typeof(Router.EndpointCallback));
 
         }
 
@@ -298,7 +330,7 @@ namespace Containers.Emission
         /// <typeparam name="T"></typeparam>
         /// <param name="il"></param>
         /// <param name="context"></param>
-        private static void EmitReturnType<T>(ILGenerator il, BuilderContext<T> context)
+        private static void EmitReturnType<M,T>(ILGenerator il, BuilderContext<M,T> context)
         {
             // If we do not have a return type from the delegate
             // Then we need to clear the stack from any target calls
@@ -307,8 +339,10 @@ namespace Containers.Emission
                 if (context.TargetReturn != typeof(void))
                 {
                     il.Emit(OpCodes.Pop); //bonk
+                    DWrite($"Pop");
                 }
                 il.Emit(OpCodes.Ret); //and return
+                DWrite($"Ret");
                 return;
             }
 
@@ -317,19 +351,23 @@ namespace Containers.Emission
             {
                 // Null is null
                 il.Emit(OpCodes.Ldnull);
+                DWrite($"ldnull");
             }
             else if (context.TargetReturn.IsValueType)
             {
                 // value types, we can box into an object
                 il.Emit(OpCodes.Box, context.TargetReturn);
+                DWrite($"Box       {context.TargetReturn}");
             }
             else
             {
                 // ref types, we can cast into objects
                 il.Emit(OpCodes.Castclass, typeof(object));
+                DWrite($"Castclass Object");
             }
 
             il.Emit(OpCodes.Ret);
+            DWrite($"Ret");
 
         }
 
@@ -344,6 +382,7 @@ namespace Containers.Emission
         {
 
             il.Emit(OpCodes.Ldarg_S, srcIndex); // load
+            DWrite($"Ldarg.S   {srcIndex}");
 
             // Handle ref types like In and Out
             if (targetType.IsByRef)
@@ -362,6 +401,7 @@ namespace Containers.Emission
                 if (targetType != entryType && targetType != typeof(object))
                 {
                     il.Emit(OpCodes.Castclass, targetType);
+                    DWrite($"Castclass {targetType.Name}");
                 }
             }
         }
@@ -382,10 +422,8 @@ namespace Containers.Emission
             else
             {
                 il.Emit(OpCodes.Unbox_Any, paramType);
-
             }
         }
-
 
         /// <summary>
         /// Handles the emission of nullable types. Should only be called for non-null underlying type of a Nullable{}
@@ -393,32 +431,22 @@ namespace Containers.Emission
         /// <param name="il"></param>
         /// <param name="underlyingType"></param>
         /// <param name="srcIndex"></param>
-        /// <remarks>
-        /// Non-primitive only require us to box whatever we get into a nullable of the type,
-        /// however primitives require null to be converted into a boxed 0 value;
-        /// <code>
-        /// if (pop == null)
-        ///     [if type is value]
-        ///         push 0        //or 0.0 for fp
-        ///     [else]            //[type is ref]
-        ///         push nullref
-        /// else: reload parameter
-        /// then: unbox           //unbox the nullable
-        /// </code>
-        /// </remarks>
         private static void HandleNullableValueType(ILGenerator il, Type underlyingType, int srcIndex)
         {
-
+            var nullableType = typeof(Nullable<>).MakeGenericType(underlyingType);
+            var nullConstructor = nullableType.GetConstructor([underlyingType])!;
+            
             if (!underlyingType.IsPrimitive)
             {
                 // This case is simple
-                var nullableType = typeof(Nullable<>).MakeGenericType(underlyingType);
                 il.Emit(OpCodes.Unbox_Any, nullableType);
+                //DWrite($"Unbox.Any {nullableType.Name}");
             }
             else
             {
                 var labelNotNull = il.DefineLabel();
                 var labelDone = il.DefineLabel();
+                var local = il.DeclareLocal(nullableType);
 
                 // Check if we retrieved "null" from the data entry
                 il.Emit(OpCodes.Brtrue_S, labelNotNull); //>---------┐
@@ -427,18 +455,33 @@ namespace Containers.Emission
                 *///so we need a zero                                |
                 var code = GetZeroForPrimitive(underlyingType);//    |
                 il.Emit(code);//                                     |
+                il.Emit(OpCodes.Newobj, nullConstructor);//          |
+                il.Emit(OpCodes.Stloc, local); //                    |
+                //                                                   |
+                //                                                   |
                 // now jump to boxing                                |
                 il.Emit(OpCodes.Br_S, labelDone); //>----------┐     |
                 //                                             |     |
                 // Non-null value                              |     |
                 il.MarkLabel(labelNotNull);         // <-------)-----┘    
                 il.Emit(OpCodes.Ldarg_S, srcIndex); //         |
+                il.Emit(OpCodes.Unbox_Any, underlyingType); // |
+                il.Emit(OpCodes.Stloc, local);//               |
+                //                                             |
                 //                                             |
                 il.MarkLabel(labelDone); //<-------------------┘
-                il.Emit(OpCodes.Unbox_Any, underlyingType); // Unbox the nullable type
+                il.Emit(OpCodes.Ldloc, local);
+
+                DWrite($"BrTrue.S  notnull");
+                DWrite($"Ldc.I4    0");
+                DWrite($"Newobj    {underlyingType.Name}");
+                DWrite($"Br.s      done");
+                DWrite($"Label     notnull");
+                DWrite($"Ldarg.S   {srcIndex}");
+                DWrite($"Unbox.Any {underlyingType.Name}");
+                DWrite($"Label     done");
+
             }
-
-
         }
 
         /// <summary>
